813a1672d602052edc7ec800d0373925e02d6496
Gerald Greyvenstein <ggreyvensteinemail@gmail.com>
2019-04-21 23:23:36 +0200

    //

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,15 +5,13 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-private __albumUrl = "../assets/album.json";
+   private __albumUrl = "../assets/album.json";
  
 constructor(private _http: Http) { }
 
    getAlbum(id: number) 
    {
-
-    return this._http.get(this.__albumUrl).map((Response) => Response.json);
-
-    
+    return this._http.get(this.__albumUrl).map((response) => 
+    response.json());
    }  
 }
