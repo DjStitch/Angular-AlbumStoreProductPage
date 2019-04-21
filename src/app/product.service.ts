d4a8b43849f0ccf4c04a973b954305196e7db8df
Gerald Greyvenstein <ggreyvensteinemail@gmail.com>
2019-04-21 23:26:10 +0200

    //

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ constructor(private _http: Http) { }
 
    getAlbum(id: number) 
    {
-    return this._http.get(this.__albumUrl).map((response) => 
+    return this._http.get(this._albumUrl).map((response) => 
     response.json());
    }  
 }
